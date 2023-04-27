917ceee0657c96eae67f5d1736fbd5cf4e1510c4
meta7834 <126189123+meta7834@users.noreply.github.com>
2023-04-27 16:38:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__filename, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 //routes
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/category", categoryRoutes);
